f0acee6c18fc0b2ad1e7a74ad9524c69982dce28
Orlewilson Maia <orlewilson.maia@gmail.com>
2019-03-14 22:50:56 -0400

    add hello world nodejs console

---
+++ 01-hello-world-nodejs-console/index.js
@@ -0,0 +1,11 @@
+/*
+  Oficina Como Criar Coisas Inteligentes com NodeJS e Arduino
+  Arduino Day Manaus 2019 - 16/03/2019
+  Facilitador:       Orlewilson Bentes Maia
+  Data Criacao:      14/03/2019
+  Data Atualizacao:  14/03/2019
+  Descricao:         Hello World NodeJS no console
+*/
+
+// mostra mensagem no console
+console.log("Hello World! Seja bem-vindo ao NodeJS!");
